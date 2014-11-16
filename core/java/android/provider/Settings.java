2c6983ddf8bd6540d6e7b8c9131ad25e88199d4d
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-11-16 19:49:53 +0200

    [1/2] base: Reboot Recovery & Reboot Download in Power Menu
    Coded from scratch by (yashade2001). It's not a port from CM12.1 or other LP ROMs.
    
    Drawables by Alex Cruz
    
    PS2:
    -use same logic as in 4.4 when to enable advanced reboot
    -compared to 4.4 dont implement in ShutdownThread but as a submenu of the global actions menu
    -expose advanced reboot API from ShutdownThread
    -add new GlobalAction filter for checking primary user and secure locksreens
    
    PS6:
    -new drawables
    
    PS7:
    -slient mode button sets no interruptions
    
    Change-Id: I3e60ce93a541b0c9deb7ad53b27825acce91d251
    
    Modified for Samsung devices by (nivlafx)
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    	services/core/java/com/android/server/power/ShutdownThread.java
    
    # Conflicts:
    #	core/java/android/provider/Settings.java
    #	core/res/res/values/strings.xml
    #	policy/src/com/android/internal/policy/impl/GlobalActions.java
    
    # Conflicts:
    #	core/java/android/provider/Settings.java
    #	core/res/res/values/symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5708,6 +5708,18 @@ public final class Settings {
                 "camera_double_tap_power_gesture_disabled";
 
         /**
+         * Whether to include options in power menu for rebooting into recovery or download
+         * @hide
+         */
+        public static final String ADVANCED_REBOOT = "advanced_reboot";
+
+        /**
+         * Whether developer settings dare enabled.
+         * @hide
+         */
+        public static final String DEVELOPER_OPTIONS_ENABLED = "developer_options_enabled";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
