2b1de19dc0c85525d8ba67819514817861153f17
Diogo Ferreira <defer@cyngn.com>
2014-12-17 17:16:16 +0000

    su: Add an indicator when a 'su' session is active
    This adds an indicator to the statusbar that is visible whenever a root
    session is active, akin to the fine location icon.
    
    Conflicts:
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java
    
    Change-Id: I669fcc87e2b7a3201308170d7727ec5fb0e00e2b
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    
    # Conflicts:
    #	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    #	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarPolicy.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -167,6 +167,7 @@ import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.PreviewInflater;
 import com.android.systemui.statusbar.policy.RotationLockControllerImpl;
 import com.android.systemui.statusbar.policy.SecurityControllerImpl;
+import com.android.systemui.statusbar.policy.SuControllerImpl;
 import com.android.systemui.statusbar.policy.UserInfoController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.ZenModeController;
@@ -262,6 +263,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     KeyguardMonitor mKeyguardMonitor;
     BrightnessMirrorController mBrightnessMirrorController;
     AccessibilityController mAccessibilityController;
+    SuControllerImpl mSuController;
 
     int mNaturalBarHeight = -1;
     int mIconSize = -1;
@@ -595,7 +597,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         addNavigationBar();
 
         // Lastly, call to the icon policy to install/update all the icons.
-        mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController, mHotspotController);
+        mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController, mHotspotController, mSuController);
         mSettingsObserver.onChange(false); // set up
 
         mHeadsUpObserver.onChange(true); // set up
@@ -822,6 +824,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mVolumeComponent = getComponent(VolumeComponent.class);
         if (mVolumeComponent != null) {
             mZenModeController = mVolumeComponent.getZenController();
+            mSuController = new SuControllerImpl(mContext);
         }
         mCastController = new CastControllerImpl(mContext);
         final SignalClusterView signalCluster =
