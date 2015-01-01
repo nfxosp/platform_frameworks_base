17f454fc1a9f0e4e4338437d040660588ed8a1fe
Alberto97 <albertop2197@gmail.com>
2015-01-01 19:06:01 +0100

    Squash 'Clear All' Button Changes
    Change-Id: Ibeb1bdbc02637cdb3e754a7a727f5a929d6ad73f
    
    Add clear all recents action to recents panel
    
    Change-Id: I2243ebc83abc483e78b112900a653d7e61edc5ba
    
    Fix - place Clear Recents button above the nav bar
    
    Change-Id: Ie7059cd1f71bae28ff25f367221b7c7ba0131932
    
    Fix up 'clear recents' UX.
    
    Use the 'dismiss all' button drawable and place the button in the upper
    right corner. Doing so fixes the broken animation when going into
    recents.
    
    Change-Id: I497a8b023f1872c5f9ae5df87af6ed2159e6b7b1
    
    SystemUI : Fix clear app data crash in recents panel
    
    Change-Id: I78d48fae2ba6ac9727d4997e24af8e4b134950ab
    
    Remove useless string
    
    Change-Id: I3c979688634239109995030ce9be666e1cd06051
    
    NEW FAB BUTTON
    
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
    	packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -27,6 +27,7 @@ import android.net.Uri;
 import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowInsets;
@@ -39,6 +40,8 @@ import com.android.systemui.recents.model.RecentsPackageMonitor;
 import com.android.systemui.recents.model.RecentsTaskLoader;
 import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
+
+import com.android.systemui.R;
 
 import java.util.ArrayList;
 
@@ -65,6 +68,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
     ArrayList<TaskStack> mStacks;
     View mSearchBar;
     RecentsViewCallbacks mCb;
+    View mClearRecents;
+    View mFloatingButton;
 
     public RecentsView(Context context) {
         super(context);
@@ -148,6 +153,17 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
 
         // Trigger a new layout
         requestLayout();
+    }
+
+    public void dismissAllTasksAnimated() {
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (child != mSearchBar) {
+                TaskStackView stackView = (TaskStackView) child;
+                stackView.dismissAllTasks();
+            }
+        }
     }
 
     /** Launches the focused task from the first stack if possible */
@@ -289,6 +305,13 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         mConfig.getTaskStackBounds(width, height, mConfig.systemInsets.top,
                 mConfig.systemInsets.right, taskStackBounds);
 
+        if (mFloatingButton != null) {
+            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)
+                    mFloatingButton.getLayoutParams();
+            params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+            mFloatingButton.setLayoutParams(params);
+        }
+
         // Measure each TaskStackView with the full width and height of the window since the 
         // transition view is a child of that stack view
         int childCount = getChildCount();
@@ -303,6 +326,27 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         }
 
         setMeasuredDimension(width, height);
+    }
+
+    public void noUserInteraction() {
+        if (mClearRecents != null) {
+            mClearRecents.setVisibility(View.VISIBLE);
+        }
+        if (mFloatingButton != null) {
+            mFloatingButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow () {
+        super.onAttachedToWindow();
+        mFloatingButton = ((View)getParent()).findViewById(R.id.floating_action_button);
+        mClearRecents = ((View)getParent()).findViewById(R.id.clear_recents);
+        mClearRecents.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dismissAllTasksAnimated();
+            }
+        });
     }
 
     /**
